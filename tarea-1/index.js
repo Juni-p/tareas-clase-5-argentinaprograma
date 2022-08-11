fffbebf6e9c4b04d226a56f4b449169fd419114b
Juni-p <66553007+Juni-p@users.noreply.github.com>
2022-08-11 14:17:38 -0300

    cambia la variable meses
    modifico el nombre de la variable para tener mayor claridad a la hora de leer el codigo

--- tarea-1/index.js
+++ tarea-1/index.js
@@ -3,7 +3,7 @@ const $btnIngresoMensual = document.querySelector(
 );
 
 $btnIngresoMensual.onclick = function () {
-  const MESES = 12;
+  const MESES_EN_UN_ANIO = 12;
   const salarioAnual = Number(document.querySelector("#salario-anual").value);
   const salarioMensual = salarioAnual / MESES;
   document.querySelector("#salario-mensual").value = salarioMensual.toFixed(2);
